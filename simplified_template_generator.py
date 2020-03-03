7ccfc64db76abc65fc48402abcdc85777596974e
paulafdzy <46695052+paulafdzy@users.noreply.github.com>
2020-03-03 16:13:14 -0600

    Update simplified_template_generator.py

--- simplified_template_generator.py
+++ simplified_template_generator.py
@@ -14,7 +14,7 @@ with open('metadata.json') as json_file:
     subject = data['Subject']
     source = data['Source']
     given_title = data['Title']
-
+    fips = data['FIPS']
 
 #get data from spreadsheet
 finaldata = pd.read_csv(dataset)
@@ -26,53 +26,48 @@ county_number= finaldata[subject].tolist()
 state = finaldata['STATE'].tolist()
 #state average, state
 
-
-#to calculate with codeeee
-state_list =
-rank = []
-state_average={}
-for this_state in state_list:
-    slst = []
-    state_total = 0
-    num_counties = 0
-    for i in range(0, len(county)):
-        if state[i] == this_state:
-            slst.append(county_number[i])
-            state_total = state_total + county_number[i]
-            num_counties = num_counties + 1
-    state_average[this_state] = state_total/ num_counties
-    state_rank.append(ss.rankdata(slst))
-
-
-
-
-
 def localized_Text(fips):
+
 
     f = int(fips)
     i = fips_code.index(f)
 
-    t = Template("The {{y}} {{s}} in {{c}}, (which is the closest county in your
-     area that collects such data), was {{cn}}, which is {{hl}} the state
-     average of {{sa}}, according to {{src}}. Within the state, {{c}} has the {{r}}
-     highest {{s}}, and it is on the {{p}} percentile in the country.")
+    #rank = [] change to rank[i] in template
 
+    slst = []
+    state_total = 0
+    num_counties = 0
+    this_state= state[i]
+    for c in range(0, len(county)):
+        if state[c] == this_state:
+            state_total = state_total + county_number[c]
+            if county_number[c] != 0:
+                num_counties = num_counties + 1
+    state_average = county_number[i]/ num_counties
+
+    #rank.append(ss.rankdata(slst))
+
+
+    t = Template("The {{y}} {{s}} in {{c}}, (which is the closest county in your area that collects such data), was {{cn}}, which is {{hl}} the state average of {{sa}}, according to {{src}}. Within the state, {{c}} has the {{r}} highest {{s}}, and it is on the {{p}} percentile in the country.")
     ti= Template(given_title)
+
     percentile = percentileofscore(county_number, county_number[i])
+    percentile = int(percentile)
+
+
 
 #generate templates
-    if county_number[i] > state_average:
-        highlow = "higher than"
-    elif county_number[i] == state_average:
-        highlow = "the same as"
+    if int(county_number[i]) > state_average:
+         highlow = "higher than"
+    elif int(county_number[i]) == state_average:
+         highlow = "the same as"
     else:
-        highlow = "lower than"
+         highlow = "lower than"
+    #highlow = "higher"
+    print(type(county_number[i]))
 
 
-    generated_text = t.render( y = year, s = subject, c = county[i],
-    cn = county_number[i], hl = highlow, sa = state_average[state[i]],
-    src = source, r = rank[i], p = percentile)
-
+    generated_text = t.render( y = year, s = subject, c = county[i], cn = county_number[i], hl = highlow, sa = state_average, src = source, r = "rank", p = percentile)
     generated_title = ti.render(c = county[i])
 
 #make file and write json obj
@@ -87,12 +82,13 @@ def localized_Text(fips):
     f.write(textjson)
 
 
-#create file for every fips code
-# for num in fips_code:
-#     if math.isnan(num):
-#         pass
-#     else:
-#         c = int(num)
-#         localized_Text(c)
-#         print("done")
+# create file for every fips code
+#  for num in fips_code:
+#      if math.isnan(num):
+#          pass
+#      else:
+#          c = int(num)
+#          localized_Text(c)
+#          print("done")
 
+localized_Text(fips)
